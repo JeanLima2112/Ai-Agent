753037901da1187a63256b2c7e2fb91bf3072f02
JeanLima2112 <jeanlima5939@gmail.com>
2025-10-04 14:45:52 -0300

    feat: base

--- main.py
+++ main.py
@@ -1,13 +1,5 @@
 from fastapi import FastAPI, Depends, HTTPException
 from fastapi.middleware.cors import CORSMiddleware
-from sqlalchemy.orm import Session
-
-from db import engine, init_db, get_db, close_db
-from models.user import User
-
-from controllers import auth_router
-
-init_db()
 
 app = FastAPI()
 
@@ -18,14 +10,3 @@ app.add_middleware(
     allow_methods=["*"],
     allow_headers=["*"],
 )
-
-app.include_router(auth_router)
-
-@app.on_event("startup")
-async def startup():
-    init_db()
-
-@app.on_event("shutdown")
-async def shutdown():
-    close_db()
-
