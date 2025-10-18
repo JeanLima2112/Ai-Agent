0f12d8dba87961819ecc04a6d4ea5ebadbb4e372
JeanLima2112 <jeanlima5939@gmail.com>
2025-10-18 16:05:06 -0300

    feat: get env api key

--- dtos/cvv_dto.py
+++
@@ -1,6 +0,0 @@
-from fastapi import UploadFile, File, Form
-from typing import Annotated
-
-class CreateCVVDTO:
-    pdf_file: Annotated[UploadFile, File(..., description="Arquivo PDF contendo o currículo")]
-    description: Annotated[str, Form(..., description="Descrição do vaga")]
--- routes/cvv_route.py
+++ routes/cvv_route.py
@@ -1,20 +1,35 @@
-from fastapi import APIRouter, HTTPException, status
+from fastapi import APIRouter, HTTPException, status, UploadFile, File, Form
 from fastapi.responses import StreamingResponse
 from dotenv import load_dotenv
-from dtos.cvv_dto import CreateCVVDTO
+import os
 
 cvv_router = APIRouter(prefix="/cvv", tags=["cvv"])
 
+load_dotenv()
+
+# llm = ChatGoogleGenerativeAI(
+#     model="models/gemini-2.5-flash",
+#     temperature=0,
+#     api_key=os.getenv("GOOGLE_API_KEY"),
+# )
+
+print(os.getenv("GOOGLE_API_KEY"))
+
 @cvv_router.post("/create-cvv", status_code=status.HTTP_200_OK)
-async def create_cvv(cvv_data: CreateCVVDTO):
-    if not cvv_data.pdf_file.filename.lower().endswith(".pdf"):
+async def create_cvv(
+    pdf_file: UploadFile = File(...),
+    description: str = Form(...)
+):
+    if not pdf_file.filename.lower().endswith(".pdf"):
         raise HTTPException(
             status_code=status.HTTP_400_BAD_REQUEST,
             detail="O arquivo deve ser um PDF"
         )
+    
 
-    return StreamingResponse(
-        cvv_data.pdf_file.file,
-        media_type="application/pdf",
-        headers={"Content-Disposition": f'attachment; filename="{cvv_data.pdf_file.filename}"'}
-    )
+
+    # return StreamingResponse(
+    #     pdf_file.file,
+    #     media_type="application/pdf",
+    #     headers={"Content-Disposition": f'attachment; filename="{pdf_file.filename}"'}
+    # )
